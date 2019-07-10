7c4a2d8014cb660e363cc5271e1451d3c7ba96a3
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-10 15:23:10 +0400

    initialized server with express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const express = require("express");
+const server = express();
+
+server.get("/", (req, res, next) => {});
+
+server.listen(5000, () => {
+  console.log("The magic is at 5000");
+});
