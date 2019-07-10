f451c08c01f79c56a3f31c368679953b5b74f057
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-10 19:47:25 +0400

    add post routes

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,9 +3,54 @@ const express = require("express");
 const userDb = require("./userDb");
 const router = express.Router();
 
-// router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const post = req.body;
+  userDb
+    .insert(post)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error adding the post"
+      });
+    });
+});
 
-// router.post("/:id/posts", (req, res) => {});
+// router.post("/:id/posts", (req, res) => {
+//   const { params, body } = req;
+//   userDb.getById(params.id)
+//     .then(result => {
+//       console.log(result[0].id);
+//       if (result[0].id > 0) {
+//         if (body.text) {
+//           const commentText = { ...body, post_id: params.id };
+//           Db.insertComment(commentText)
+//             .then(result => {
+//               console.log("happy path");
+//               console.log(result);
+//               res.status(201).json(body);
+//             })
+//             .catch(err => {
+//               res.status(404).json({ errorMessage: "Can't find that id!" });
+//             });
+//         } else {
+//           res.status(400).json({
+//             errorMessage: "Please provide text for the comment."
+//           });
+//         }
+//       } else {
+//         res.status(404).json({
+//           message: "The post with the specified ID does not exist."
+//         });
+//       }
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ errorMessage: "Can't find the comment in the post" });
+//     });
+// });
 
 router.get("/", (req, res) => {
   userDb
@@ -82,14 +127,17 @@ router.delete("/:id", (req, res) => {
 router.put("/:id", (req, res) => {
   const { id } = req.params.id;
   const user = req.body;
-  userDb.update(id, user).then(result => {
-    res.status(200).json(result);
-  }).catch(err => {
-    res.status(500).json({
-      message: "Error updating the post"
+  userDb
+    .update(id, user)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error updating the post"
+      });
     });
 });
-
 //custom middleware
 
 // function validateUserId(req, res, next) {}
